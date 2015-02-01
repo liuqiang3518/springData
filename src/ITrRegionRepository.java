9cb2d197dec8a21b74a34fb0d4e3b2b776fa3aa4
liuqiang3518 <liuqiang3518@hotmail.com>
2015-02-01 09:53:59 +0800

    Revert "repository"
    This reverts commit 7def93c0dc4008f0b5a0c3ca4e02fa888e8dc71c.

--- src/ITrRegionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.logeti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.logeti.model.TrRegion;
-
-public interface ITrRegionRepository extends JpaRepository<TrRegion,Integer>{
-}
